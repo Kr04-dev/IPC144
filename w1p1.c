bf6653030208f31ec1586a9c85db0fc3be3b080a
Kr04-dev <133912939+Kr04-dev@users.noreply.github.com>
2023-07-19 13:59:08 -0400

    Add files via upload

---
+++ w1p1.c
@@ -0,0 +1,32 @@
+/*/////////////////////////////////////////////////////////////////////////
+                          Workshop - #1 (P1)
+Full Name  :Timothy Lee
+Student ID#:177133212
+Email      :glee89@myseneca.ca
+Section    :NRA
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+/////////////////////////////////////////////////////////////////////////*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+
+int main(void) {
+
+    printf("Workshop 1 Part-1\n");
+    printf("=================\n\n");
+    printf("I'm displaying this using the 'printf' stdio\n");
+    printf("(standard input output) library function!\n\n");
+    printf("Dear teacher,\n\n");
+    printf("  I promise I will work hard from this day onward.\n");
+    printf("  I acknowledge that practice is extremely important,\n");
+    printf("  so I will do all workshops, quizzes, and assignments.\n\n");
+    printf("Sincerely,\n\n");
+    printf("Timothy Lee");
+    printf("Student ID# 177133212");
+
+    return 0;
+}
